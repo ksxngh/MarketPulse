27f39140e4ad6c40ee8e7c15892abfe8b82155fb
Karanveer <ks2328679@gmail.com>
2026-05-25 16:27:05 -0700

    inngest debug

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -6,6 +6,8 @@ import {
   sendSignUpEmail,
 } from "@/inngest/functions";
 
+export const maxDuration = 60;
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [sendSignUpEmail, dailyNewsSummary, checkPriceAlerts],
